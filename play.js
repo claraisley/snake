3e3ae813d7e753ba9dcaf9c3c0203d4493767575
claraisley <isleyc97@gmail.com>
2019-12-04 20:34:12 +0000

    Added input file

--- play.js
+++ play.js
@@ -1,21 +1,9 @@
 
 
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 console.log('Connecting ...');
 
-const setupInput = function() {
   connect();
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-const handleUserInput = stdin.on('data', (key) => {
-  if (key === '\u0003') {
-    process.exit()
-  } 
-});
-  return stdin;
-};
 
 setupInput();
